b40689cb471eb015a3660ecf12d5ae3a5e650946
koobdev <gupnodo21@gmail.com>
2021-12-13 17:54:42 +0900

    SpringCore Studying (2021-12-13)

---
+++ SpringCore/src/test/java/hello/core/singleton/statefulServiceTest.java
@@ -0,0 +1,34 @@
+package hello.core.singleton;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class statefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        // ThreadA : A사용자가 10000원 주문
+        statefulService1.order("userA", 10000);
+        // ThreadB : B사용자가 20000원 주문
+        statefulService2.order("userB", 20000);
+
+        // ThreadA : 사용자 A가 주문금액을 조회
+        int price = statefulService1.getPrice();
+    }
+
+    static class TestConfig{
+
+        @Bean
+        public StatefulService statefulService(){
+            return new StatefulService();
+        }
+    }
+}
